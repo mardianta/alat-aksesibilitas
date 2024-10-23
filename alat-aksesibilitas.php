afe8b49f406919647c5cfdbf0ae12c0d7a510efb
Sevenguardians Alfian <design.amirfian@gmail.com>
2024-10-23 11:06:21 +0700

    Update alat-aksesibilitas.php

--- alat-aksesibilitas.php
+++ alat-aksesibilitas.php
@@ -26,14 +26,14 @@ if (! defined('ABSPATH')) {
 function enqueue_accessibility_scripts()
 {
   // Perbaikan di sini: Memanggil plugin_dir_url() di dalam fungsi
-  wp_enqueue_style('alat-aksesibilitas-style', plugin_dir_url(__FILE__) . 'alat-aksesibilitas.css');
-  wp_enqueue_script('alat-aksesibilitas-script', plugin_dir_url(__FILE__) . 'alat-aksesibilitas.js', array('jquery'), '1.0', true);
+  wp_enqueue_style('alat-aksesibilitas-style', plugin_dir_url(__FILE__) . 'css/alat-aksesibilitas.css');
+  wp_enqueue_script('alat-aksesibilitas-script', plugin_dir_url(__FILE__) . 'js/alat-aksesibilitas.js', array('jquery'), '1.0', true);
 }
 add_action('wp_enqueue_scripts', 'enqueue_accessibility_scripts');
 
 // Menampilkan Toolbar Aksesibilitas
 function display_accessibility_toolbar()
 {
-  include(plugin_dir_path(__FILE__) . 'toolbar.php');
+  include(plugin_dir_path(__FILE__) . 'assets/toolbar.php');
 }
 add_action('wp_footer', 'display_accessibility_toolbar');
