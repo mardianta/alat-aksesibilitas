66fcec39c6be24dc91de135911f38b106d684b20
Sevenguardians Alfian <design.amirfian@gmail.com>
2024-10-23 07:42:20 +0700

    Update toolbar.php

--- toolbar.php
+++ toolbar.php
@@ -1,4 +1,63 @@
 <div id="accessibility-toolbar">
   <h2>Alat Aksesibilitas</h2>
   <button id="changeFontSizeBtn">Ubah Ukuran Font</button>
+  
+  <div class="widget-container">
+    <h2>Alat Aksesibilitas</h2>
+        <div class="widget-header">
+            <h1>Widget kebesaran</h1>
+            <button class="close-btn">&times;</button>
+        </div>
+        <div class="widget-grid">
+            <div class="widget-item">
+                <i class="fas fa-adjust"></i>
+                <span>Kontras +</span>
+            </div>
+            <div class="widget-item">
+                <i class="fas fa-link"></i>
+                <span>Sorot tautan</span>
+            </div>
+            <div class="widget-item">
+                <i class="fas fa-text-height"></i>
+                <span>Teks Lebih Besar</span>
+            </div>
+            <div class="widget-item">
+                <i class="fas fa-arrows-alt-h"></i>
+                <span>Spasi teks</span>
+            </div>
+            <div class="widget-item">
+                <i class="fas fa-spinner"></i>
+                <span>Animasi dijeda</span>
+            </div>
+            <div class="widget-item">
+                <i class="fas fa-image"></i>
+                <span>Sembunyikan Gambar</span>
+            </div>
+            <div class="widget-item">
+                <i class="fas fa-info-circle"></i>
+                <span>Ramah Disleksia</span>
+            </div>
+            <div class="widget-item">
+                <i class="fas fa-mouse-pointer"></i>
+                <span>Kursor</span>
+            </div>
+            <div class="widget-item">
+                <i class="fas fa-comment-alt"></i>
+                <span>Keterangan alat</span>
+            </div>
+            <div class="widget-item">
+                <i class="fas fa-text-height"></i>
+                <span>Tinggi garis</span>
+            </div>
+            <div class="widget-item">
+                <i class="fas fa-align-left"></i>
+                <span>Perataan Teks</span>
+            </div>
+            <div class="widget-item">
+                <i class="fas fa-tint"></i>
+                <span>Kejenuhan</span>
+            </div>
+        </div>
+    </div>
+
 </div>
